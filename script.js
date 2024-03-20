2cbea7aa7c11eea1d6e5adc3ac90f3fd9fb15b3a
Guruputra-Developer <rguruputra@gmail.com>
2024-03-20 17:43:38 +0530

    adding frontend web page

---
+++ script.js
@@ -0,0 +1,8 @@
+let heading=document.querySelector("h1")
+
+heading.onclick=()=>{
+    let body=document.querySelector("h1")
+    let p=document.createElement("p")
+    p.innerHTML="Be happy and live succesful life"
+    body.appendChild(p)
+}
